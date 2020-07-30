46ccedbb52a99645daae3991901e0bfc1328a207
David Pellegrini <dap@davidpellegrini.com>
2020-07-30 13:31:44 -0400

    Swtich to Fragment layout

--- app/src/main/java/ca/davidpellegrini/tipcalculator_s20/SettingsFragment.java
+++ app/src/main/java/ca/davidpellegrini/tipcalculator_s20/SettingsFragment.java
@@ -1,19 +1,31 @@
 package ca.davidpellegrini.tipcalculator_s20;
 
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
 
-import androidx.fragment.app.Fragment;
+import android.app.Fragment;
 
 import android.preference.PreferenceFragment;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class SettingsFragment extends PreferenceFragment {
+public class SettingsFragment extends PreferenceFragment
+implements OnSharedPreferenceChangeListener{
 
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.preferences);
+    }
+
+    public void onSharedPreferenceChanged(SharedPreferences savedValues, String key){
+        Fragment tipFragment = getFragmentManager()
+                .findFragmentById(R.id.main_fragment);
+
+        if(tipFragment != null){
+            tipFragment.onResume();
+        }
     }
 }
